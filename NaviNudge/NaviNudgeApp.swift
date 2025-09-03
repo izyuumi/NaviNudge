0fe38e1afbf2c9be5d10218713b29ca20c45871b
Yumi Izumi <mail@yumi.to>
2025-09-03 17:52:36 +0900

    Initial Commit

---
+++ NaviNudge/NaviNudgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NaviNudgeApp.swift
+//  NaviNudge
+//
+//  Created by Yumi Izumi on 2025/09/03.
+//
+
+import SwiftUI
+
+@main
+struct NaviNudgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
